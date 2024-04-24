07b0aaad8a597045ab238fa8033c6d7f3cc67e78
Noé Tarbouriech <noe.tarbouriech@etu.umontpellier.fr>
2024-04-24 11:03:45 +0200

    feat(infra): add prod setup using nginx

--- infra/import.sh
+++ infra/import.sh
@@ -1,9 +1,9 @@
 # create index
-curl -XPOST http://127.0.0.1:7280/api/v1/indexes --header "content-type: application/yaml" --data-binary @./beer-index.yaml
+curl -XPOST http://127.0.0.1:3000/api/v1/indexes --header "content-type: application/yaml" --data-binary @./beer-index.yaml
 
 # populate index
 for file in ./beer-dataset/beer-database/*.json
 do
   data=$(jq -c '.data[]' "$file")
-  curl -XPOST -H "Content-Type: application/json" "http://127.0.0.1:7280/api/v1/beer/ingest?commit=force" -d "$data"
+  curl -XPOST -H "Content-Type: application/json" "http://127.0.0.1:3000/api/v1/beer/ingest?commit=force" -d "$data"
 done
